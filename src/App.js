d5531c2f3c9c30504df76644cbd5889745ff83eb
heriyantojob <heriyantojob@gmail.com>
2020-12-17 18:34:52 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className=" bg-red-200">
+        tes
+      </div>
+    </div>
+  );
+}
+
+export default App;
